c11908264a4607448738b440146271ab0bf8b4e7
Young soo Kim <qhdl301@gmail.com>
2016-09-24 19:15:26 +0900

    4번과제 입니다

--- B04.java
+++ B04.java
@@ -1,31 +1,37 @@
 import java.util.*;
-class B01{
+class B04{
 	
-	int birth_year;
-	int age;
-
+	int kor;
+	int eng;
+	int math;
+	
 	public static void main(String[] s){
-	new B01().input();
+	new B04().input();
 	}
         
 	void input(){
 	Scanner s= new Scanner(System.in);
-	System.out.println("태어난년도를 적으시오.");
-	this.birth_year=s.nextInt();
-	this.age = 2014 - birth_year;
+	System.out.println("국어점수를 입력하세요");
+	this.kor=s.nextInt();
+	System.out.println("영어점수를 입력하세요");
+	this.eng=s.nextInt();
+	System.out.println("수학점수를 입력하세요");
+	this.math=s.nextInt();
 	print();
 
 	}
 
 	void print(){
 	
-	if(this.age <=20){
-	System.out.println("미성년자입니다.");
-	}
-	else
-	{
-	System.out.println("성인입니다.");
-	}
-	
+	int total= this.kor+this.eng+this.math;
+	System.out.println("입력하신 점수의 총점은"+total+"이고");
+	double avg= total/3.0;
+	System.out.println("평균은"+avg+ "입니다");
+        if(this.kor>=90)
+	System.out.println("국어 점수가 우수합니다");
+        if(this.eng>=90)
+	System.out.println("영어 점수가 우수합니다");
+        if(this.math>=90)
+	System.out.println("수학 점수가 우수합니다");
        }
       }		
