4b60d9c648846d777ddc532d8fd4d9bcf86c83a2
Young soo Kim <qhdl301@gmail.com>
2016-09-24 19:28:57 +0900

    과제5번 입니다

--- B05.java
+++ B05.java
@@ -1,31 +1,40 @@
 import java.util.*;
-class B01{
+class B05{
 	
 	int birth_year;
 	int age;
 
 	public static void main(String[] s){
-	new B01().input();
+	new B05().input();
 	}
         
 	void input(){
 	Scanner s= new Scanner(System.in);
-	System.out.println("태어난년도를 적으시오.");
+	System.out.println("태어난년도를 입력하시오.");
 	this.birth_year=s.nextInt();
-	this.age = 2014 - birth_year;
 	print();
-
 	}
 
 	void print(){
 	
-	if(this.age <=20){
-	System.out.println("미성년자입니다.");
+	this.age = 2012 - this.birth_year + 1;
+	if(age<7){
+	System.out.println("유아입니다");
+	}	
+	else if(age<13){
+	System.out.println("어린이입니다");
 	}
-	else
-	{
-	System.out.println("성인입니다.");
+	else if(age<20){
+	System.out.println("청소년입니다");
 	}
-	
+	else if(age<30){
+	System.out.println("청년입니다");
+	}
+	else if(age<60){
+	System.out.println("중년입니다");
+	}
+	else{
+	System.out.println("노년입니다");
+	}
        }
       }		
